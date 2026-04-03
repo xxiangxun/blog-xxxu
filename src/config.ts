b16349f4c3ddabb5dcaf1f60b86b740424b0f250
louis <112264916+xxiangxun@users.noreply.github.com>
2026-04-04 00:37:05 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://xxxu.com.cn/", // replace this with your deployed domain
+  author: "xxxu",
+  profile: "https://xxxu.com.cn/",
+  desc: "记录技术学习、编程实践与生活思考，分享学习方法、成长复盘与个人感悟，在技术精进与生活平衡中持续进步，做长期主义的成长记录者。",
+  title: "xxxu-me",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -12,12 +12,12 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "zh-CN", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
