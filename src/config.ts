278141982c5e1a09f686ad55e05121d263a4a51a
louis <112264916+xxiangxun@users.noreply.github.com>
2026-04-08 09:53:35 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export const SITE = {
   title: "xxxu-me",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 10,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
